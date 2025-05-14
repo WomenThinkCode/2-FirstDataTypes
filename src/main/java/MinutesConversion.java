f672171d956377f1fdb1a6d3f07ec699f71d5f03
tspilanjhb024 <tcpilane01@gmail.com>
2025-05-14 13:39:51 +0200

    Created Minutes Conversion file

---
+++ src/main/java/MinutesConversion.java
@@ -0,0 +1,23 @@
+
+import javax.swing.JOptionPane;
+
+
+
+public class MinutesConversion {
+    public static void main(String[] args) {
+        String input =  JOptionPane.showInputDialog("Enter minutes:");
+        try {
+            int minutes = Integer.parseInt(input);
+            int days = minutes / 1440;
+            int remainingDays = minutes % 1440;
+            int hours = remainingDays / 60;
+            int finalMinutes = remainingDays % 60;
+            String message = minutes + " minutes = " + days + " day(s), " 
+                           + hours + " hour(s), and " + finalMinutes + " minute(s)";
+            JOptionPane.showMessageDialog(null, message);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Invalid input. Please enter a valid number");
+        }
+
+    }
+}
